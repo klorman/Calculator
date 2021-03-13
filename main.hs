863584ebd6401a49bc2d5accdb3320054a64b5a5
klorman <38407912+klorman@users.noreply.github.com>
2021-03-14 02:37:41 +0300

    Update main.hs

--- main.hs
+++ main.hs
@@ -1,14 +1,9 @@
-import Debug.Trace
-
 type Operator = Double -> Double -> Double
 type Entry = (String, Operator)
 type Register = [Entry]
 
-
-
 operatorRegister :: Register
 operatorRegister = [
-        (".", undefined),
         ("(", undefined),
         (")", undefined),
         ("[", undefined),
@@ -39,17 +34,21 @@ numbers (x:xs) prev
         rest  = numbers xs False
         nrest = numbers xs True
 
+isNumber :: String -> Bool
+isNumber str =
+    case (reads str) :: [(Double, String)] of
+        [(_, "")] -> True
+        _         -> False
 
 parenthesis :: Register -> [String] -> [String]
-parenthesis _ [""] = error "ERROR: incorrect placement of parentheses"
+parenthesis _ [""] = error "ERROR: Invalid parenthesis placement!"
 parenthesis ((operator, function):rest) unparsed =
     case span (/= operator) unparsed of
         (_, []) -> parenthesis rest unparsed
-        --([""], _) -> []   
         (left, right)
             | operator == "(" -> parenthesis operatorRegister (left ++ parenthesis operatorRegister (tail right))
             | operator == ")" -> ([show (evaluate operatorRegister left)]) ++ (tail right)
-            | otherwise -> error "ERROR: incorrect placement of parentheses"
+            | otherwise -> error "ERROR: Invalid parenthesis placement!"
 
 calculate :: String -> String
 calculate str = show ( evaluate operatorRegister $ numbers str False)
@@ -58,12 +57,17 @@ evaluate :: Register -> [String] -> Double
 evaluate _ [left, right]
     | left == "+" =   evaluate operatorRegister [right]
     | left == "-" = - evaluate operatorRegister [right]
-    | otherwise = error "ERROR: incorrect use of the operator"
+    | left == "(" || left == ")" || right == "(" || right == ")" = error "ERROR: Invalid parenthesis placement!"
+    | left == "*" || left == "/" || left == "^" = error "ERROR: Invalid use of the operator!"
+    | otherwise = error "ERROR: Invalid value!"
 
-evaluate _ [number] = read number
+evaluate _ [number]
+    | isNumber number = read number
+    | otherwise = error "ERROR: Invalid value!"
+
 evaluate ((operator, function):rest) unparsed =
     case span (/= operator) unparsed of
-        (_, []) -> trace (show unparsed) evaluate rest unparsed
+        (_, []) -> evaluate rest unparsed
         (left, right)
             |  operator == "(" -> evaluate operatorRegister (left ++ (parenthesis operatorRegister $ tail right))
             | otherwise -> function 
