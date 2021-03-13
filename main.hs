b4a326c31a286335c0bac0123451f229694e274d
klorman <38407912+klorman@users.noreply.github.com>
2021-03-14 01:04:27 +0300

    bug fixes

--- main.hs
+++ main.hs
@@ -1,5 +1,4 @@
 import Debug.Trace
-import Control.Applicative ((<$>), (<*>))
 
 type Operator = Double -> Double -> Double
 type Entry = (String, Operator)
@@ -21,22 +20,25 @@ operatorRegister = [
         ("^", (**))
     ] 
 
-numbers :: String -> [String]
-numbers [] = [""]
-numbers (x:xs) 
-        | x == ' ' = rest
-        | x == '+' = "" : "+" : rest
-        | x == '-' = "" : "-" : rest
-        | x == '*' = "" : "*" : rest
-        | x == '/' = "" : "/" : rest
-        | x == '^' = "" : "^" : rest
-        | x == '(' =      "(" : rest
-        | x == ')' = "" : ")" : rest
-        | x == '[' = "" : "[" : rest
-        | x == ']' = "" : "]" : rest
-        | otherwise = (x : head rest) : tail rest
+numbers :: String -> Bool -> [String]
+numbers [] True  = [""]
+numbers [] False = []
+numbers (x:xs) prev
+        | x == ' ' = numbers xs prev
+        | x == '+' = if prev then "" : "+" : rest else "+" : rest
+        | x == '-' = if prev then "" : "-" : rest else "-" : rest
+        | x == '*' = if prev then "" : "*" : rest else "*" : rest
+        | x == '/' = if prev then "" : "/" : rest else "/" : rest
+        | x == '^' = if prev then "" : "^" : rest else "^" : rest
+        | x == '(' = if prev then "" : "(" : rest else "(" : rest
+        | x == ')' = if prev then "" : ")" : rest else ")" : rest
+        | x == '[' = if prev then "" : "[" : rest else "[" : rest
+        | x == ']' = if prev then "" : "]" : rest else "]" : rest
+        | otherwise = (x : head nrest) : tail nrest
     where
-        rest = numbers xs
+        rest  = numbers xs False
+        nrest = numbers xs True
+
 
 parenthesis :: Register -> [String] -> [String]
 parenthesis _ [""] = error "ERROR: incorrect placement of parentheses"
@@ -46,20 +48,27 @@ parenthesis ((operator, function):rest) unparsed =
         --([""], _) -> []   
         (left, right)
             | operator == "(" -> parenthesis operatorRegister (left ++ parenthesis operatorRegister (tail right))
-            | operator == ")" -> ([show (evaluate operatorRegister left)]) ++ (drop 2 right)
+            | operator == ")" -> ([show (evaluate operatorRegister left)]) ++ (tail right)
             | otherwise -> error "ERROR: incorrect placement of parentheses"
 
 calculate :: String -> String
-calculate = show . evaluate operatorRegister . numbers
+calculate str = show ( evaluate operatorRegister $ numbers str False)
 
 evaluate :: Register -> [String] -> Double
+evaluate _ [left, right]
+    | left == "+" =   evaluate operatorRegister [right]
+    | left == "-" = - evaluate operatorRegister [right]
+    | otherwise = error "ERROR: incorrect use of the operator"
+
 evaluate _ [number] = read number
 evaluate ((operator, function):rest) unparsed =
     case span (/= operator) unparsed of
         (_, []) -> trace (show unparsed) evaluate rest unparsed
         (left, right)
-            | operator == "(" -> evaluate operatorRegister (left ++ (parenthesis operatorRegister $ tail right))
-            | otherwise -> function (evaluate operatorRegister left) (evaluate operatorRegister $ tail right)
+            |  operator == "(" -> evaluate operatorRegister (left ++ (parenthesis operatorRegister $ tail right))
+            | otherwise -> function 
+            (evaluate operatorRegister left) 
+            (evaluate operatorRegister $ tail right)
 
 main :: IO()
 main = interact (unlines . (map calculate) . lines)
